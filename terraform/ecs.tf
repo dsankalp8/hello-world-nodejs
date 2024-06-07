bda0313618e5f9ead8fd9451e0fbe2f66a303b55
dsankalp8 <dsankalp8@gmail.com>
2024-06-08 02:51:17 +0530

    Adding all files

---
+++ terraform/ecs.tf
@@ -0,0 +1,60 @@
+resource "aws_ecs_cluster" "main" {
+  name = var.app_name
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.app_name}-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = var.app_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.app_name
+      image     = "YOUR_DOCKER_IMAGE"
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "main" {
+  name            = var.app_name
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = var.subnet_ids
+    security_groups = [var.security_group_id]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  default     = "hello-world-nodejs"
+}
+
+variable "container_port" {
+  description = "Port on which the container listens"
+  type        = number
+  default     = 3000
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "Security group ID"
+  type        = string
+}
